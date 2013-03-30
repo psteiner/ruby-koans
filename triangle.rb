c8a5caacc8d538805a5c6877fc34468b25fd54f1
Philip Steiner <philip@psteiner.com>
2013-03-29 21:14:37 -0700

    about triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && a == c && b == c
-    return :equilateral
-  elsif a == b && b != c && a != c || b == c && a != b && a != c || a == c && a != b && b != c
-    return :isosceles
-  elsif a != b && a != c && b != c
-    return :scalene
+  raise TriangleError if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError if (a + b <= c) || (a + c <= b) || (b + c <= a)
+  if a == b && a == c 
+    :equilateral
+  elsif a == b || b == c || a == c 
+     :isosceles
+  else
+    :scalene
   end
 end
 
